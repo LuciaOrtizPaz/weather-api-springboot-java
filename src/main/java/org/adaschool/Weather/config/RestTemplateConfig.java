b397a9e936683b1c0a870a2a4d503c393559e00b
Lucia Izabel Ortiz Paz <luciaizabelortizpaz556@gmail.com>
2024-07-31 18:59:09 -0600

    first commit

--- src/main/java/org/adaschool/Weather/config/RestTemplateConfig.java
+++ src/main/java/org/adaschool/Weather/config/RestTemplateConfig.java
@@ -1,2 +1,16 @@
-package org.adaschool.Weather.config;public class RestTemplateConfig {
+package org.adaschool.Weather.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
+
+
--- src/main/java/org/adaschool/Weather/service/WeatherReportService.java
+++ src/main/java/org/adaschool/Weather/service/WeatherReportService.java
@@ -2,17 +2,24 @@ package org.adaschool.Weather.service;
 
 import org.adaschool.Weather.data.WeatherApiResponse;
 import org.adaschool.Weather.data.WeatherReport;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public class WeatherReportService {
 
-    private static final String API_KEY = "your-api-key";
+    private static final String API_KEY = "5f58fd4cdbaa237e65828421fdd73d78";
     private static final String API_URL = "https://api.openweathermap.org/data/2.5/weather";
 
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public WeatherReportService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     public WeatherReport getWeatherReport(double latitude, double longitude) {
-        RestTemplate restTemplate = new RestTemplate();
         String url = API_URL + "?lat=" + latitude + "&lon=" + longitude + "&appid=" + API_KEY;
         WeatherApiResponse response = restTemplate.getForObject(url, WeatherApiResponse.class);
 
